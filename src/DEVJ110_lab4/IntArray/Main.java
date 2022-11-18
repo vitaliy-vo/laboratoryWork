1c386aedec690b8461797555e8ddf60a5286dd5d
Vitaliy <Yakovlev3100@yandex.ru>
2022-11-18 10:27:33 +0300

    laboratory work 4

--- src/DEVJ110_lab4/IntArray/Main.java
+++ src/DEVJ110_lab4/IntArray/Main.java
@@ -7,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         /*Создаем целочисленый массив*/
-         Binary binary = new Binary();
+
+        Binary binary = new Binary();
+
 
 
         System.out.println("Печатаем целочисленный массив");
