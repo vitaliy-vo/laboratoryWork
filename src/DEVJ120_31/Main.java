91eba4598f956ba429c7f4e585ca98d362a5d466
Vitaliy <Yakovlev3100@yandex.ru>
2023-01-13 18:02:29 +0300

    laboratory work  DEVJ120_3.1

---
+++ src/DEVJ120_31/Main.java
@@ -0,0 +1,11 @@
+package DEVJ120_31;
+
+import java.net.MalformedURLException;
+
+public class Main {
+    public static void main(String[] args)  {
+        Calculator calculator= new Calculator();
+
+
+    }
+}
